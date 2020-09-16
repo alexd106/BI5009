85f6c06296029e95b5bd214203d5503153604bc9
alexd106 <alexd106@gmail.com>
2020-09-16 21:25:04 +0100

    minor typo fix in reveal code

--- reveal.R
+++ reveal.R
@@ -2,6 +2,8 @@
 show_text12 <- TRUE    # ex 1 and 2
 show_text34 <- TRUE    # ex 3 and 4
 show_text5 <- TRUE    # ex 5
+show_text_GDE <- TRUE    # ex GDE
+
 
 # Change to TRUE to show lectures
 show_lecture <- TRUE
