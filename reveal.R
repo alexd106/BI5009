d095bb444aab93ad633c125bcb18604d8ed9e923
alexd106 <alexd106@gmail.com>
2020-10-07 08:28:18 +0100

    GDE exercise solutions reveal

--- reveal.R
+++ reveal.R
@@ -2,7 +2,7 @@
 show_text12 <- TRUE    # ex 1 and 2
 show_text34 <- TRUE    # ex 3 and 4
 show_text5 <- TRUE    # ex 5
-show_text_GDE <- FALSE    # ex GDE
+show_text_GDE <- TRUE    # ex GDE
 show_text_lm1 <- FALSE    # ex LM1
 show_text_lm2 <- FALSE    # ex LM2
 
