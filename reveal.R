703f146f71af3f64a5e886b7c98972b93e93b88d
alexd106 <alexd106@gmail.com>
2020-10-13 10:52:21 +0100

    add lm1 exercise solutions

--- reveal.R
+++ reveal.R
@@ -3,11 +3,14 @@ show_text12 <- TRUE    # ex 1 and 2
 show_text34 <- TRUE    # ex 3 and 4
 show_text5 <- TRUE    # ex 5
 show_text_GDE <- TRUE    # ex GDE
-show_text_lm1 <- FALSE    # ex LM1
+show_text_lm1 <- TRUE    # ex LM1
 show_text_lm2 <- FALSE    # ex LM2
 show_text_lm3 <- FALSE   # ex LM3
 show_text_lm4 <- FALSE    # ex LM4
-
+show_text_glm1 <- FALSE    # ex GLM1
+show_text_glm2 <- FALSE    # ex GLM2
+show_text_glm3 <- FALSE    # ex GLM3
+show_text_glm4 <- FALSE    # ex GLM4
 
 # Change to TRUE to show lectures
 show_lecture <- TRUE
